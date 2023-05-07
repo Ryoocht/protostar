c8fe1e01c7311bc005a496d749b5ca876fd8f337
Ryoocht <ryo1.t.zubu@gmail.com>
2023-05-07 21:45:04 +1000

    configured: mysql connection

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    await prisma.$connect()
+    console.log('Connected to the database')
+  } catch (e) {
+    console.error('Error connecting to the database', e)
+  } finally {
+    await prisma.$disconnect()
+  }
+
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
