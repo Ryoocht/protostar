2808b091159630399b64e9cd69ab99810d6c80df
Ryoocht <ryo1.t.zubu@gmail.com>
2023-05-14 20:17:53 +1000

    added: student module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { ConfigModule } from '@nestjs/config'
+import { StudentModule } from './modules/student/student.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), StudentModule],
   controllers: [AppController],
 })
 export class AppModule {}
